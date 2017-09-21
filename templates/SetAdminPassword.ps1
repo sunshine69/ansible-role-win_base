76e837c2ad70b4d2d3dde7ca312f94d0b1ac5932
Steve Kieu <steve.kieu@xvt.com.au>
2017-09-22 09:49:00 +1000

    Remove filebeat installation
    Filebeat config depends on the log_files and it is different per role_type thus better not in win_base.
    So win_base now only has:
    
    - ec2 launch init if it is ec2 instance
    - winlogbeat subsribe for basic event Application, System and Security
    - Password init and changed for user admin.

--- templates/SetAdminPassword.ps1
+++ templates/SetAdminPassword.ps1
@@ -1,3 +1,2 @@
 $admin = [adsi]("WinNT://./administrator, user")
 $admin.PSBase.Invoke("SetPassword", "{{ admin_password }}")
-# Invoke-Expression ((New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1'))
